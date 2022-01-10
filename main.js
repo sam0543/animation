8e43cb022c52a7bcb93d27304eac16410fd00717
Sam <63824440+sam0543@users.noreply.github.com>
2022-01-09 17:57:46 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+var gameEngine = new GameEngine();
+
+var ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./00robotspritesheet.png");
+
+
+ASSET_MANAGER.downloadAll(function () {
+	var canvas = document.getElementById('gameWorld');
+	var ctx = canvas.getContext('2d');
+
+	gameEngine.init(ctx);
+
+	gameEngine.addEntity(new Mario(gameEngine, 50, 50, ASSET_MANAGER.getAsset("./00robotspritesheet.png")));
+
+	gameEngine.start();
+});
