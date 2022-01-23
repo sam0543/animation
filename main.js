9918e6316b648cf97caf46525ed41b18445ed0cb
Sam <63824440+sam0543@users.noreply.github.com>
2022-01-23 14:25:31 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ var gameEngine = new GameEngine();
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./idlemovecombospritesheet.png");
+ASSET_MANAGER.queueDownload("./background.png");
 
 
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
+	
 
 	gameEngine.init(ctx);
+	gameEngine.start();
 
 	gameEngine.addEntity(new Robot(gameEngine, 0, 0, ASSET_MANAGER.getAsset("./idlemovecombospritesheet.png")));
-
-	gameEngine.start();
+	
+	
 });
