b1d50cafeba803ba6e12374036b569449b8bdbb5
Sam Wong <63824440+sam0543@users.noreply.github.com>
2022-01-23 15:45:55 -0800

    a

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 class Background {
     constructor(game, x, y, spritesheet) {
         Object.assign(this, { game, x, y });
+        this.spritesheet = ASSET_MANAGER.getAsset("./background.png");
         
     };
     
+    update(){};
+    
     draw(ctx) {
-                
-        ctx.drawImage(this.spritesheet);
-        
-        
+        ctx.drawImage(this.spritesheet, 0, 0, 1368, 801);
     };
 };
